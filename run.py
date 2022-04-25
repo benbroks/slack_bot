03e4d2e1a4fb1235192fa88707a444a7dd66cbfb
Ben Brooks <bjbrooks@usc.edu>
2022-04-25 10:07:26 -0700

    no messages on the weekend

--- run.py
+++ run.py
@@ -1,16 +1,23 @@
 from slack_sdk.webhook import WebhookClient
+import requests
 
 import os
 
+time_url = "https://worldtimeapi.org/api/timezone/America/New_York"
+
 if __name__ == '__main__':
     url = os.environ['WEBHOOK_URL']
-
-    webhook = WebhookClient(url)
-    t = """Respond to this thread with the following:
-    - What did you do yesterday?
-    - What do you plan to do today?
-    - What, if anything, is preventing you from completing your tasks?
-    """
-    response = webhook.send(text=t)
+    
+    tz_check = requests.get(time_url)
+    day_of_week = tz_check.json().get('day_of_week')
+    
+    if day_of_week >= 1 and day_of_week <= 5:
+        webhook = WebhookClient(url)
+        t = """Respond to this thread with the following:
+        - What did you do yesterday?
+        - What do you plan to do today?
+        - What, if anything, is preventing you from completing your tasks?
+        """
+        response = webhook.send(text=t)
 
 
